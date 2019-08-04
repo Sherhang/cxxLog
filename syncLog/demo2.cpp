4a8eccdf859b89cd3e76f15120ab03a71abe48fd
Sherhang <Sherhang@github.com>
2019-08-04 19:15:47 +0800

    test

--- syncLog/demo2.cpp
+++ syncLog/demo2.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 void display(int id)
 {
-    for(int i=0;i<1e2;++i)
+    for(int i=0;i<1e3;++i)
     {
         DEBUG(<<"This is a test! ID is "<<id<<endl);
     }
@@ -17,12 +17,12 @@ void display(int id)
 int main()
 {
     LOGINIT("app_log","demo2");
-    thread th[10];
-    for(int i=0;i<10;++i)
+    thread th[1000];
+    for(int i=0;i<1000;++i)
     {
         th[i] = thread(display,i);
     }
-    for(int i=0;i<10;++i)
+    for(int i=0;i<1000;++i)
     {
         th[i].join();
     }
